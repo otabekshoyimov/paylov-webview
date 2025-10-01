924162e9a6d8fd7dc8ced9532d2b3f068b28d3bf
otabekshoyimov <otabek_s28@icloud.com>
2025-10-01 17:51:14 +0500

    Update App styles

--- src/index.tsx
+++ src/index.tsx
@@ -101,7 +101,7 @@ export function BottoSheet() {
         </fetcher.Form>
       </main>
       <footer className="flex w-full flex-col gap-8 pb-16">
-        <div className="mx-auto max-w-[300px] text-center text-sm text-zinc-200">
+        <div className="mx-auto max-w-[300px] text-center text-xs text-zinc-200">
           Davom etish orqali siz{" "}
           <span className="font-medium text-white">
             Foydalanish shartlariga{" "}
