0dc182c8cbe0415d199b1b01a55b634586b377df
otabekshoyimov <otabek_s28@icloud.com>
2025-10-05 11:03:04 +0500

    Remove App.tsx

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-export function App() {
-  return (
-    <>
-      <p className="read-the-docs bg-red-400 text-blue-200">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}
